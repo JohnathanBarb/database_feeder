0b1367767e925ce4eb0dc8cb960c05c40bb9bfa4
Johnathan <jbarbosa0504@gmail.com>
2021-06-08 15:13:19 -0300

    added argument path to command line

--- database.py
+++ database.py
@@ -1,36 +1,53 @@
 from sqlalchemy.engine import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import insert, delete
+import sys
+
 
 engine = create_engine('sqlite:////home/johnathan/Documents/Projetos/feirinha/db.sqlite3', echo=True)
 Base = declarative_base()
 Base.metadata.reflect(engine)
 
+
 class Produto(Base):
     __table__ = Base.metadata.tables['hortifruti_produto']
 
 
-if __name__ == "__main__":
-    table = Produto
+def deletetable(table, engine):
     stmt = delete(table)
     with engine.begin() as conn:
         conn.execute(stmt)
-    path = "/home/johnathan/Documents/Projetos/hortifruti_scrapping/output/""20210606_hortifrutiestrela.csv"
-    with open(path, 'r') as _file:
+
+
+def inserttable(table, engine, csv):
+    with open(csv, 'r') as _file:
         _file.readline()
+        deletetable(table, engine)
         cont = 1
+        stmts = []
         while True:
-            linha = _file.readline()            
+            linha = _file.readline()
             if not linha:
                 break
             infos = str(linha).strip().split(';')
             ref = infos[0]
             descricao = infos[1]
             preco = infos[2].replace(',', '.')
-            stmt = (
+            stmts.append( (
                 insert(table).
                 values(id=cont, ref_code=ref, descricao=descricao, preco=preco)
-            )
-            with engine.begin() as conn:
-                conn.execute(stmt)
+            ))
             cont += 1
+        with engine.begin() as conn:
+            for stmt in stmts:
+                conn.execute(stmt)
+
+
+if __name__ == "__main__":
+    if len(sys.argv) > 1:
+        csvpath = sys.argv[1]
+    else:
+        folderpath = "/home/johnathan/Documents/Projetos/hortifruti_scrapping/"
+        filepath = "output/20210605_hortifrutiestrela.csv"
+        csvpath = f'{folderpath}{filepath}'
+    inserttable(Produto, engine, csvpath)          
